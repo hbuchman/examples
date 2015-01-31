71795ec9c663cc63e35928b4d2d34f991a1b040f
Joe Zachary <zachary@cs.utah.edu>
2015-01-30 22:06:41 -0700

    PS2 Grading Tests added

--- PS2Skeleton/PS2GradingTests/GradingTests.cs
+++ PS2Skeleton/PS2GradingTests/GradingTests.cs
@@ -1,6 +1,4 @@
-﻿// NOTE: These tests are not yet complete!
-
-using System;
+﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using SpreadsheetUtilities;
@@ -15,21 +13,21 @@ namespace GradingTests
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test1()
         {
-            Formula f = new Formula("+");
+            Formula f = new Formula("        ");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test2()
         {
-            Formula f = new Formula("2+5+");
+            Formula f = new Formula("((2 + 5))) + 8");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test3()
         {
-            Formula f = new Formula("2+5*7)");
+            Formula f = new Formula("2+5*8)");
         }
 
         [TestMethod()]
@@ -43,63 +41,83 @@ namespace GradingTests
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test5()
         {
-            Formula f = new Formula("5x");
+            Formula f = new Formula("+3");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test6()
         {
-            Formula f = new Formula("5+5x");
+            Formula f = new Formula("-y2");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test7()
         {
-            Formula f = new Formula("5+7+(5)8");
+            Formula f = new Formula("*7");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test8()
         {
-            Formula f = new Formula("5 5");
+            Formula f = new Formula("/z8");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test9()
         {
-            Formula f = new Formula("5 + * 3");
+            Formula f = new Formula(")");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test10()
         {
-            Formula f = new Formula("");
+            Formula f = new Formula("(*5)");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test11()
         {
-            Formula f = new Formula("((((((((((2)))))))))");
+            Formula f = new Formula("2 5");
         }
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaFormatException))]
         public void Test12()
         {
-            Formula f = new Formula("+3");
+            Formula f = new Formula("x5 y7");
         }
 
+        [TestMethod()]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Test13()
+        {
+            Formula f = new Formula("((((((((((2)))))))))");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Test14()
+        {
+            Formula f = new Formula("x");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void Test15()
+        {
+            Formula f = new Formula("x5 + x6 + x7 + (x8) +");
+        }
 
         // Simple tests that throw FormulaEvaluationExceptions
         [TestMethod()]
         [ExpectedException(typeof(FormulaEvaluationException))]
-        public void Test13()
+        public void Test16()
         {
             Formula f = new Formula("2+X1");
             f.Evaluate(s => { throw new ArgumentException(); });
@@ -107,7 +125,7 @@ namespace GradingTests
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaEvaluationException))]
-        public void Test14()
+        public void Test17()
         {
             Formula f = new Formula("5/0");
             f.Evaluate(s => 0);
@@ -115,39 +133,130 @@ namespace GradingTests
 
         [TestMethod()]
         [ExpectedException(typeof(FormulaEvaluationException))]
-        public void Test15()
+        public void Test18()
         {
             Formula f = new Formula("(5 + X1) / (X1 - 3)");
             f.Evaluate(s => 3);
         }
 
+        [TestMethod()]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Test19()
+        {
+            Formula f = new Formula("x1 + x2");
+            f.Evaluate(s => { if (s == "x1") return 0; else throw new ArgumentException(); });
+        }
 
-        
+        [TestMethod()]
+        [ExpectedException(typeof(FormulaEvaluationException))]
+        public void Test20()
+        {
+            Formula f = new Formula("x1 + x2 * x3 + x4 * x5 * x6 + x7");
+            f.Evaluate(s => { if (s == "x7") throw new ArgumentException(); else return 1; });
+        }
+
+        // Simple formulas
+        [TestMethod()]
+        public void Test21()
+        {
+            Formula f = new Formula("4.5e1");
+            Assert.AreEqual(45, f.Evaluate(s => 0), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test22()
+        {
+            Formula f = new Formula("a0");
+            Assert.AreEqual(10, f.Evaluate(s => 10), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test23()
+        {
+            Formula f = new Formula("5 + x6");
+            Assert.AreEqual(9, f.Evaluate(s => 4), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test24()
+        {
+            Formula f = new Formula("5 - x6");
+            Assert.AreEqual(1, f.Evaluate(s => 4), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test25()
+        {
+            Formula f = new Formula("5 * x6");
+            Assert.AreEqual(20, f.Evaluate(s => 4), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test26()
+        {
+            Formula f = new Formula("8 / x6");
+            Assert.AreEqual(2, f.Evaluate(s => 4), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test27()
+        {
+            Formula f = new Formula("(5 + 4) * 2");
+            Assert.AreEqual(18, f.Evaluate(s => 0), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test28()
+        {
+            Formula f = new Formula("1 + 2 + 3 * 4 + 5");
+            Assert.AreEqual(20, f.Evaluate(s => 0), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test29()
+        {
+            Formula f = new Formula("(1 + 2 + 3 * 4 + 5) * 2");
+            Assert.AreEqual(40, f.Evaluate(s => 0), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test30()
+        {
+            Formula f = new Formula("((((((((((((3))))))))))))");
+            Assert.AreEqual(3, f.Evaluate(s => 0), 1e-6);
+        }
+
+        [TestMethod()]
+        public void Test31()
+        {
+            Formula f = new Formula("((((((((((((x7))))))))))))");
+            Assert.AreEqual(7, f.Evaluate(s => 7), 1e-6);
+        }
 
         // Some more complicated formula evaluations
         [TestMethod()]
-        public void Test29()
+        public void Test32()
         {
             Formula f = new Formula("y1*3-8/2+4*(8-9*2)/14*x7");
             Assert.AreEqual(5.14285714285714, (double)f.Evaluate(s => (s == "x7") ? 1 : 4), 1e-9);
         }
 
         [TestMethod()]
-        public void Test30()
+        public void Test33()
         {
             Formula f = new Formula("x1+(x2+(x3+(x4+(x5+x6))))");
             Assert.AreEqual(6, (double)f.Evaluate(s => 1), 1e-9);
         }
 
         [TestMethod()]
-        public void Test31()
+        public void Test34()
         {
             Formula f = new Formula("((((x1+x2)+x3)+x4)+x5)+x6");
             Assert.AreEqual(12, (double)f.Evaluate(s => 2), 1e-9);
         }
 
         [TestMethod()]
-        public void Test32()
+        public void Test35()
         {
             Formula f = new Formula("a4-a4*a4/a4");
             Assert.AreEqual(0, (double)f.Evaluate(s => 3), 1e-9);
@@ -155,81 +264,81 @@ namespace GradingTests
 
         // Tests to make sure there can be more than one formula at a time
         [TestMethod()]
-        public void Test51a()
+        public void Test36()
         {
-            Formula f1 = new Formula("2");
-            Formula f2 = new Formula("3");
-            Assert.IsTrue(f1.ToString().IndexOf("2") >= 0);
-            Assert.IsTrue(f2.ToString().IndexOf("3") >= 0);
+            Formula f1 = new Formula("x5+3");
+            Formula f2 = new Formula("x5-3");
+            Assert.AreEqual(6, f1.Evaluate(s => 3), 1e-6);
+            Assert.AreEqual(0, f2.Evaluate(s => 3), 1e-6);
         }
 
         [TestMethod()]
-        public void Test51b()
+        public void Test37()
         {
-            Formula f1 = new Formula("2");
-            Formula f2 = new Formula("3");
-            Assert.IsTrue(f1.ToString().IndexOf("2") >= 0);
-            Assert.IsTrue(f2.ToString().IndexOf("3") >= 0);
+            Formula f1 = new Formula("x5+3");
+            Formula f2 = new Formula("x5-3");
+            Assert.AreEqual(6, f1.Evaluate(s => 3), 1e-6);
+            Assert.AreEqual(0, f2.Evaluate(s => 3), 1e-6);
         }
 
         [TestMethod()]
-        public void Test51c()
+        public void Test38()
         {
-            Formula f1 = new Formula("2");
-            Formula f2 = new Formula("3");
-            Assert.IsTrue(f1.ToString().IndexOf("2") >= 0);
-            Assert.IsTrue(f2.ToString().IndexOf("3") >= 0);
+            Formula f1 = new Formula("x5+3");
+            Formula f2 = new Formula("x5-3");
+            Assert.AreEqual(6, f1.Evaluate(s => 3), 1e-6);
+            Assert.AreEqual(0, f2.Evaluate(s => 3), 1e-6);
         }
 
         [TestMethod()]
-        public void Test51d()
+        public void Test39()
         {
-            Formula f1 = new Formula("2");
-            Formula f2 = new Formula("3");
-            Assert.IsTrue(f1.ToString().IndexOf("2") >= 0);
-            Assert.IsTrue(f2.ToString().IndexOf("3") >= 0);
+            Formula f1 = new Formula("x5+3");
+            Formula f2 = new Formula("x5-3");
+            Assert.AreEqual(6, f1.Evaluate(s => 3), 1e-6);
+            Assert.AreEqual(0, f2.Evaluate(s => 3), 1e-6);
         }
 
         [TestMethod()]
-        public void Test51e()
+        public void Test40()
         {
-            Formula f1 = new Formula("2");
-            Formula f2 = new Formula("3");
-            Assert.IsTrue(f1.ToString().IndexOf("2") >= 0);
-            Assert.IsTrue(f2.ToString().IndexOf("3") >= 0);
+            Formula f1 = new Formula("x5+3");
+            Formula f2 = new Formula("x5-3");
+            Assert.AreEqual(6, f1.Evaluate(s => 3), 1e-6);
+            Assert.AreEqual(0, f2.Evaluate(s => 3), 1e-6);
         }
 
         // Stress test for constructor
         [TestMethod()]
-        public void Test52a()
+        public void Test41()
         {
             Formula f = new Formula("(((((2+3*X1)/(7e-5+X2-X4))*X5+.0005e+92)-8.2)*3.14159) * ((x2+3.1)-.00000000008)");
         }
 
         // Stress test for constructor
         [TestMethod()]
-        public void Test52b()
+        public void Test42()
         {
             Formula f = new Formula("(((((2+3*X1)/(7e-5+X2-X4))*X5+.0005e+92)-8.2)*3.14159) * ((x2+3.1)-.00000000008)");
         }
 
         // Stress test for constructor
         [TestMethod()]
-        public void Test52c()
+        public void Test43()
         {
             Formula f = new Formula("(((((2+3*X1)/(7e-5+X2-X4))*X5+.0005e+92)-8.2)*3.14159) * ((x2+3.1)-.00000000008)");
         }
 
         // Stress test for constructor
         [TestMethod()]
-        public void Test52d()
+        public void Test44()
         {
             Formula f = new Formula("(((((2+3*X1)/(7e-5+X2-X4))*X5+.0005e+92)-8.2)*3.14159) * ((x2+3.1)-.00000000008)");
         }
 
         // Stress test for constructor
         [TestMethod()]
-        public void Test52e()
+        public void Test45()
         {
             Formula f = new Formula("(((((2+3*X1)/(7e-5+X2-X4))*X5+.0005e+92)-8.2)*3.14159) * ((x2+3.1)-.00000000008)");
         }
