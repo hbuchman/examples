ed6546b34c54959c460a6c3239fec02e2b533807
Joe Zachary <zachary@cs.utah.edu>
2015-02-24 16:02:51 -0700

    Changes from class

--- VariousExamples/AsyncMethods/Form1.cs
+++ VariousExamples/AsyncMethods/Form1.cs
@@ -29,38 +29,54 @@ namespace AsyncMethods
                 Task<BigInteger> task = Task<BigInteger>.Run(() => Tasks.Factorial(n));
                 factorialBox.Text = task.Result.ToString();
             }
+            else
+            {
+                factorialBox.Text = "Not an int";
+            }
         }
 
-        //private CancellationTokenSource tokenSource;
+        private CancellationTokenSource tokenSource;
 
         private async void FactorialButtonClickAsync(object sender, EventArgs e)
         {
+            tokenSource = new CancellationTokenSource();
             int n;
             if (int.TryParse(valueOfN.Text, out n))
             {
                 button3.Enabled = true;
                 button2.Enabled = false;
                 factorialBox.Clear();
-                Task<BigInteger> task = Task<BigInteger>.Run(() => Factorial(n));
-                BigInteger result = await task;
-                factorialBox.Text = result.ToString();
+                Task<BigInteger> task = Task<BigInteger>.Run(() => Factorial(n, tokenSource.Token), tokenSource.Token);
+                try
+                {
+                    BigInteger result = await task;
+                    factorialBox.Text = result.ToString();
+                }
+                catch (OperationCanceledException)
+                {
+                    factorialBox.Text = "Cancelled";
+                }
                 button3.Enabled = false;
                 button2.Enabled = true;
+                // This belongs in a finally block:
+                tokenSource.Dispose();
             }
         }
 
         private void CancelButtonClick(object sender, EventArgs e)
         {
+            tokenSource.Cancel();
         }
 
         /// <summary>
         /// Returns n!
         /// </summary>
-        private static BigInteger Factorial(int n)
+        private static BigInteger Factorial(int n, CancellationToken cancel)
         {
             BigInteger result = 1;
             while (n > 0)
             {
+                cancel.ThrowIfCancellationRequested();
                 result *= n;
                 n--;
             }
--- VariousExamples/FactorialTester/Properties/AssemblyInfo.cs
+++ VariousExamples/FactorialTester/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hash_Set_vs_Dictionary")]
+[assembly: AssemblyTitle("FactorialTester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hash_Set_vs_Dictionary")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("FactorialTester")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("416221b9-cd46-4aae-91c1-43f2055e6cc6")]
+[assembly: Guid("5b0772a8-7197-42e2-ba94-a25239e5f5cf")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
