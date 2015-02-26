b928f6015684caa2e2ba63d56e1d02480940b95f
Joe Zachary <zachary@cs.utah.edu>
2015-02-26 16:45:38 -0700

    Added a bunch of projects

---
+++ MoreExamples/WebScraperSynchronous/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WebScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scrapeButton = new System.Windows.Forms.Button();
+            this.tagBox = new System.Windows.Forms.TextBox();
+            this.charCount = new System.Windows.Forms.Label();
+            this.charCountBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // scrapeButton
+            // 
+            this.scrapeButton.Location = new System.Drawing.Point(48, 33);
+            this.scrapeButton.Name = "scrapeButton";
+            this.scrapeButton.Size = new System.Drawing.Size(239, 23);
+            this.scrapeButton.TabIndex = 0;
+            this.scrapeButton.Text = "Scrape";
+            this.scrapeButton.UseVisualStyleBackColor = true;
+            this.scrapeButton.Click += new System.EventHandler(this.scrapeButtonClick);
+            // 
+            // tagBox
+            // 
+            this.tagBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tagBox.Location = new System.Drawing.Point(12, 128);
+            this.tagBox.Multiline = true;
+            this.tagBox.Name = "tagBox";
+            this.tagBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tagBox.Size = new System.Drawing.Size(337, 192);
+            this.tagBox.TabIndex = 1;
+            // 
+            // charCount
+            // 
+            this.charCount.AutoSize = true;
+            this.charCount.Location = new System.Drawing.Point(45, 80);
+            this.charCount.Name = "charCount";
+            this.charCount.Size = new System.Drawing.Size(81, 17);
+            this.charCount.TabIndex = 2;
+            this.charCount.Text = "Char count:";
+            // 
+            // charCountBox
+            // 
+            this.charCountBox.Location = new System.Drawing.Point(158, 75);
+            this.charCountBox.Name = "charCountBox";
+            this.charCountBox.Size = new System.Drawing.Size(129, 22);
+            this.charCountBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 332);
+            this.Controls.Add(this.charCountBox);
+            this.Controls.Add(this.charCount);
+            this.Controls.Add(this.tagBox);
+            this.Controls.Add(this.scrapeButton);
+            this.Name = "Form1";
+            this.Text = "Web Page Tag Scraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button scrapeButton;
+        private System.Windows.Forms.TextBox tagBox;
+        private System.Windows.Forms.Label charCount;
+        private System.Windows.Forms.TextBox charCountBox;
+    }
+}
+
---
+++ MoreExamples/WebScraperTasks/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Sites being scraped
+        private string[] SITES =
+        {
+            "http://google.com/",
+            "http://www.utah.edu/",
+            "http://www.cs.utah.edu/"
+        };
+
+        // Regex used to recognize tags (not foolproof!)
+        private Regex TAG_REGEX = new Regex("<([a-zA-Z0-9]+)");
+
+        /// <summary>
+        /// Run when the Scape button is clicked
+        /// </summary>
+        private void scrapeButtonClick(object sender, EventArgs e)
+        {
+            // Reset some stuff
+            tags = new SortedDictionary<string, int>();
+            scrapeButton.Enabled = false;
+            charCountBox.Text = "";
+            tagBox.Text = "";
+
+            // Fire off separate tasks to scrape the various sites
+            List<Task<int>> tasks = new List<Task<int>>();
+            foreach (string url in SITES)
+            {
+                tasks.Add(Task.Run(() => scrapeTags(url)));
+            }
+
+            // Create a task to wait for the tasks to be completed and
+            // then finish up.
+            Task.Run(() => finishScrapeButtonClick(tasks));
+        }
+
+        private void finishScrapeButtonClick (List<Task<int>> tasks)
+        {
+            // Wait for all the tasks to finish
+            Task.WaitAll(tasks.ToArray());
+
+            // Add up and display the lengths
+            int sum = 0;
+            foreach (Task<int> t in tasks)
+            {
+                sum += t.Result;
+            }
+
+            // Updating a UI element must be done on the GUI event
+            // thread.  Thus, we have to use the Invoke method,
+            // which exists for this purpose.
+            charCountBox.Invoke((Action) (() => charCountBox.Text = sum.ToString()));
+
+            // Display the tag counts and re-enable scrape button
+            charCountBox.Invoke((Action) displayTagCounts);
+            scrapeButton.Invoke((Action) (() => scrapeButton.Enabled = true));
+        }
+
+        /// <summary>
+        /// Returns the number of characters in the referenced web page,
+        /// and updates tag counts in the shared tags dictionary
+        /// </summary>
+        private int scrapeTags(string url)
+        {
+            // Get the web page in a synchronous fashion
+            HttpClient client = new HttpClient();
+            String result = client.GetStringAsync(url).Result;
+
+            // Count all the different kinds of tags on the page
+            foreach (Match m in TAG_REGEX.Matches(result))
+            {
+                updateTagCounts(m.Groups[1].ToString());
+                //Task.Delay(1).Wait();
+            }
+
+            // Return the length of the page
+            return result.Length;
+        }
+
+        // Maps tags to the number of times they occur.
+        // Not that this is shared by all the scraping tasks.
+        private SortedDictionary<string, int> tags;
+
+        // Used for synchronization
+        private readonly object sync = new object();
+
+        /// <summary>
+        /// Updates the count for the specified tag
+        /// </summary>
+        private void updateTagCounts(String tag)
+        {
+            lock (sync)
+            {
+                int count;
+                if (tags.TryGetValue(tag, out count))
+                {
+                    tags[tag] = count + 1;
+                }
+                else
+                {
+                    tags[tag] = 1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Displays the counts for all the tags
+        /// </summary>
+        private void displayTagCounts()
+        {
+            foreach (string tag in tags.Keys)
+            {
+                tagBox.AppendText(tag + " " + tags[tag] + "\r\n");
+            }
+        }
+    }
+}
